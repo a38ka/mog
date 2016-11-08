d4937699a2cd5c3cd05b42463db7df16c93b1dc0
a38ka <a38ka.system@gmail.com>
2016-11-08 23:00:59 +0900

    コマンドループ実装中

---
+++ main.py
@@ -0,0 +1,13 @@
+#coding:utf-8
+
+
+cmd = input("コマンドを入力してください\n[1]ゲームスタート[2]続きから[3]やめる\n => ")
+
+if cmd == "1":
+    print("1")
+elif cmd == "2":
+    print("2")
+elif cmd == "3":
+    print("3")
+else:
+    print("None:")
