0ab39cf273319c69089bded20fe7854ae01d45bf
a38ka <a38ka.system@gmail.com>
2016-11-30 14:05:22 +0900

    Add Method

---
+++ main.rb
@@ -0,0 +1,16 @@
+#!/usr/bin/env ruby
+#encoding:utf-8
+
+print "GET DOWN / BackStreetBoys\n"
+
+command = nil
+
+command = gets().chomp()
+
+def mainLoop(cmd)
+  print "Method Load\n"
+  print "Type : #{cmd}\n"
+end
+
+
+mainLoop(command)
