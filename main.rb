2188046ab613f25a8aa02519700861eb43ac7a47
a38ka <a38ka.system@gmail.com>
2016-12-01 22:20:51 +0900

    prottype Start

--- main.rb
+++ main.rb
@@ -1,16 +1,5 @@
 #!/usr/bin/env ruby
 #encoding:utf-8
 
-print "GET DOWN / BackStreetBoys\n"
-
-command = nil
-
-command = gets().chomp()
-
-def mainLoop(cmd)
-  print "Method Load\n"
-  print "Type : #{cmd}\n"
-end
-
-
-mainLoop(command)
+print "Program Start...."
+print ""
