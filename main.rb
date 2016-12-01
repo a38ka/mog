f9348a5bff9a6d4311569d931b7cb3e2cd96d54e
a38ka <a38ka.system@gmail.com>
2016-12-02 02:42:41 +0900

    麻雀

--- main.rb
+++ main.rb
@@ -1,9 +1,13 @@
 #!/usr/bin/env ruby
 #encoding:utf-8
 
-print "Program Start...."
+print "Program Start....\n"
 
 command = nil
 
 print "#{command}"
 
+print "|東|南|西|北|發|白|中|\n"
+print "|牌|牌|牌|牌|牌|牌|牌|\n"
+print "|壱|二|三|四|五|六|七|八|九|\n"
+print "|萬|萬|萬|萬|萬|萬|萬|萬|萬|\n"
